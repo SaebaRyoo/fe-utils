b1e2a61dce08ff8a042814f56e50c5f8800fcf64
SaebaRyoo <yuanddmail@163.com>
2022-12-12 20:52:53 +0800

    fix: 修改rollup配置导致的git action job失败

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser'
+const resolve = require('@rollup/plugin-node-resolve');
+const typescript = require('@rollup/plugin-typescript');
+const commonjs = require('@rollup/plugin-commonjs');
+const { terser } = require('rollup-plugin-terser')
 
-export default [
+module.exports = [
 
   {
     input: './src/index.ts',
