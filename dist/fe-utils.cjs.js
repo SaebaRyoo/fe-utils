347827037b772eaee7306b7464fddef9cab54f53
SaebaRyoo <yuanddmail@163.com>
2022-12-12 17:13:17 +0800

    feat: 修改配置,添加CI,自动发布npm包

--- dist/fe-utils.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function sum(...args) {
-    return args.reduce((prev, total) => total + prev, 0);
-}
-
-exports.sum = sum;
--- dist/fe-utils.esm.js
+++
@@ -1,5 +0,0 @@
-function sum(...args) {
-    return args.reduce((prev, total) => total + prev, 0);
-}
-
-export { sum };
